ab36dfd383031fe20288604199f3aad467810bd1
XiRdnethFr <yg2540@nyu.edu>
2022-09-29 07:00:47 +0800

    Update_1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float horizontalMove;
+    public float force;
+
+    bool grounded = false;
+    public float castDist = 0.2f;
+
+    public float jumpHeight;
+    public float speedLimit;
+
+    bool jump = false;
+    bool doublejump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(doublejump || grounded)
+            {
+                jump = true;
+            }
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        HorizontalMove(horizontalMove);
+
+        if (jump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight));
+            jump = false;
+            if (!grounded && doublejump)
+            {
+                doublejump = false;
+            }
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist);
+        if(hit.collider != null)
+        {
+            grounded = true;
+            doublejump = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        if(Input.GetAxis("Horizontal") != 0 && rb.velocity.x != 0)
+        {
+            if(rb.velocity.x > 0)
+            {
+                
+            }
+        }
+
+    }
+
+    void HorizontalMove(float toMove)
+    {
+        float moveX = toMove * Time.fixedDeltaTime * force;
+        //rb.velocity = new Vector3(moveX, rb.velocity.y);
+        if(Mathf.Abs(rb.velocity.x) < speedLimit)
+        {
+            rb.AddForce(transform.right * moveX, ForceMode2D.Impulse);
+        }
+    }
+}
